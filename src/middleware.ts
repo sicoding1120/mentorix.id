d18be22bbe53b3be983723ab3bbf3ec892e189f2
ahfary <afabqory@gmail.com>
2024-09-25 16:21:34 +0700

    gak jadi hapus middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware (req: NextRequest) {
-//     return NextResponse.redirect(new URL("/auth/login", req.url))
-// }
+export function middleware (req: NextRequest) {
+    return NextResponse.redirect(new URL("/auth/login", req.url))
+}
 
-// export const config = {
-//     matcher: ['/dashboard/:path*'],
-// }
+export const config = {
+    matcher: ['/dashboard/:path*'],
+}

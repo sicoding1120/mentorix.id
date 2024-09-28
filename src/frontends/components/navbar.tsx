788dc23c24c40ca710b70a718d54249605fb1841
ahfary <afabqory@gmail.com>
2024-09-28 19:57:18 +0700

    navbar lagi

--- src/frontends/components/navbar.tsx
+++ src/frontends/components/navbar.tsx
@@ -11,6 +11,7 @@ import Cookies from "js-cookie";
 
 export default function NavbarEl() {
   const cookie = Cookies.get("user_id");
+
   return (
     <Navbar className="bg-color-primary bg-opacity-80 text-white py-2">
       <NavbarBrand className="gap-2">
@@ -42,10 +43,7 @@ export default function NavbarEl() {
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <label className="swap swap-rotate">
-            {/* this hidden checkbox controls the state */}
             <input type="checkbox" className="theme-controller" value="dark" />
-
-            {/* sun icon */}
             <svg
               className="swap-off h-8 w-8 fill-current"
               xmlns="http://www.w3.org/2000/svg"
@@ -53,47 +51,60 @@ export default function NavbarEl() {
             >
               <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
             </svg>
-
-            {/* moon icon */}
             <svg
               className="swap-on h-8 w-8 fill-current"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
-              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05A1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
             </svg>
           </label>
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#" className="text-white">
-            Sign In
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link href="#" className="text-white btn btn-success">
-            Sign Up
-          </Link>
-        </NavbarItem>
+        {!cookie && (
+          <>
+            <NavbarItem className="hidden lg:flex">
+              <Link href="#" className="text-white">
+                Sign In
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="#" className="text-white btn btn-success">
+                Sign Up
+              </Link>
+            </NavbarItem>
+          </>
+        )}
         {cookie && (
           <NavbarItem>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </NavbarItem>
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-circle avatar">
+                <Image
+                  src="/assets/images/avatar.png" // Avatar user
+                  alt="Avatar"
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                />
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </NavbarItem>
         )}
       </NavbarContent>
     </Navbar>

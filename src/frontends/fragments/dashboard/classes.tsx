e0d99db273c3263c20922267d4b196629a9a8bf0
ahfary <afabqory@gmail.com>
2024-09-28 20:05:19 +0700

    courses dashboard

--- src/frontends/fragments/dashboard/classes.tsx
+++ src/frontends/fragments/dashboard/classes.tsx
@@ -1,10 +1,12 @@
 import Class from '@/frontends/components/classComp'
 import ContainerClass from '@/frontends/components/containerClass'
+import NavbarEl from '@/frontends/components/navbar'
 import React from 'react'
 
 const Classes = () => {
   return (
     <div className="w-full h-full">
+      <NavbarEl />
     <Class isClasses={true} />
   </div>
   )

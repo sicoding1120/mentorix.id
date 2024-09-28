5965789e260b8aa8f9d4caa7d6943820a07ebe7e
ahfary <afabqory@gmail.com>
2024-09-28 11:38:47 +0700

    middlewarenya ketinggalan

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware (req: NextRequest) {
-    // return NextResponse.redirect(new URL("/auth/login", req.url))
-    return NextResponse.next()
+    return NextResponse.redirect(new URL("/auth/login", req.url))
+    // return NextResponse.next()
 }
 
 export const config = {

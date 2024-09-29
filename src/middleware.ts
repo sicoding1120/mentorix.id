a592cf6e43d0e581fde10c72772550eaf5718129
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2024-09-29 13:24:23 +0700

    update navbar

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import Cookies from "js-cookie";
 
-export function middleware (req: NextRequest) {
-    return NextResponse.redirect(new URL("/auth/login", req.url))
+export function middleware(req: NextRequest) {
+    const statusLogin = Cookies.get('status_login')
+    if (statusLogin === "true") {
+        return NextResponse.next()
+    } else {
+        return NextResponse.redirect(new URL("/auth/login", req.url))
+    }
 }
 
 export const config = {
-    matcher: ['/dashboard/'],
+    matcher: ['/dashboard/', '/dashboard/:path*', '/blog/:path*', '/courses/:path*'],
 }
